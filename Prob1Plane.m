47a54680d5a8dd48fd1dda06993803fd66c15267
Anand Idris <30372144+aidris823@users.noreply.github.com>
2020-02-03 14:16:03 -0500

    Fixed quotes from LaTeX

--- Prob1Plane.m
+++ Prob1Plane.m
@@ -3,7 +3,7 @@ y2 = linspace(-10,10,100);
 [XX,YY] = meshgrid(x2,y2);
 z = (-2*XX) + (-1*YY) +1.5;
 surf(XX,YY,z);
-xlabel(’x’)
-ylabel(’y’)
-zlabel(’z’)
-title(’Visualize z= 2x- y- 1.5’)
+xlabel('x')
+ylabel('y')
+zlabel('z')
+title('Visualize z= 2x- y- 1.5')
