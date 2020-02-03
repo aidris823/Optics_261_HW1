89db860e2570e40764fd1ee5fe05a17778fdc298
Anand Idris <30372144+aidris823@users.noreply.github.com>
2020-02-03 14:12:09 -0500

    Create Prob1Plane.m

---
+++ Prob1Plane.m
@@ -0,0 +1,9 @@
+x2 = linspace(-10,10,100);
+y2 = linspace(-10,10,100);
+[XX,YY] = meshgrid(x2,y2);
+z = (-2*XX) + (-1*YY) +1.5;
+surf(XX,YY,z);
+xlabel(’x’)
+ylabel(’y’)
+zlabel(’z’)
+title(’Visualize z= 2x- y- 1.5’)
