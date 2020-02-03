83d1b2d3b5dfdde53ff71efe34e090522b6549be
Anand Idris <30372144+aidris823@users.noreply.github.com>
2020-02-03 14:15:20 -0500

    Fixed quote marks that LaTeX screws up

--- Prob1C.m
+++ Prob1C.m
@@ -2,14 +2,14 @@
 x = linspace(-15,15,1000); %Creates a row vector
 ...from -10 to 10 with 1000 elements
 f1 = x*-2 + 2; %Creates a vector with scalar multiplication ...
--2 and +2
+... -2 and +2
 f2 = x*-2 +4;
-plot(x,f1,’b’)
+plot(x,f1'b')
 hold on
-plot(x,f2,’r’)
-xlabel(’x’)
-ylabel(’f’)
-title(’Visualize 2X+Y = 2’)
+plot(x,f2,'r')
+xlabel('x')
+ylabel('f')
+title('Visualize 2X+Y = 2')
 axis([-15 15 -10 10])
 %Drawing a Cartesian plane
 xline(0);
